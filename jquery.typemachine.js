e7f6f8d4efa473cf9176ef11ee2fba989890f3c3
Michal Kuklis <michal.kuklis@gmail.com>
2011-04-26 21:25:59 -0400

    init commit

---
+++ jquery.typemachine.js
@@ -0,0 +1,37 @@
+/**
+ * typemachine - Simple jQuery plugin which simulates live typing.
+ *
+ * Copyright(c) 2011 Michal Kuklis <michal.kuklis@gmail.com>
+ * MIT Licensed
+ */
+
+(function($){
+  $.fn.typemachine = function(options) {
+    // default settings
+    var settings = {
+      text: "",
+      speed: 100
+    };
+      
+    return this.each(function() {
+      if (options) {
+        $.extend(settings, options);
+      }
+
+      var that = this,
+        i = 0,
+        l = settings.text.length;
+        
+      // reset current text
+      $(this).text('');
+      var interval = setInterval(function() {
+        if (i < l) {
+          $(that).append(settings.text[i++]);
+        }
+        else {
+          clearInterval(interval);
+        }
+      }, settings.speed);
+    });
+  }
+})(jQuery);
